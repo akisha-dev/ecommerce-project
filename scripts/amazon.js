47bfb3b40b56ed1a91c94a9aba6f0d8029ee9937
Akanksha <priyaakanksha031@gmail.com>
2025-12-20 13:06:22 +0530

    make the cart quatity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -77,8 +77,18 @@ document.querySelectorAll('.js-add-to-cart')
          cart.push({
             productId: productId,
             quantity:1
-         })}
+         })};
+         let cartQuantity = 0;
+         cart.forEach((item) => {
+          cartQuantity+=item.quantity;
+
+         }) ;
+         document.querySelector('.js-cart-quantity')
+         .innerHTML=cartQuantity;
+         console.log(cartQuantity);
          console.log(cart)
     })
+
+
 
 })
