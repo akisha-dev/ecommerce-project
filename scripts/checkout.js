a53afd257c1f6f359599009f9af38be671c50b62
Akanksha <priyaakanksha031@gmail.com>
2025-12-26 10:57:06 +0530

    fix price formatting and radio selectors

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import{cart} from '../data/cart.js';
 import{products} from '../data/products.js';
+import { formatCurrency } from './utils/money.js';
 let cartSummaryHtml ='';
 cart.forEach((cartItem)=>{//for each cart iem we are going to generate html
 const productId = cartItem.productId;
@@ -25,7 +26,7 @@ cartSummaryHtml+=
     ${matchingProduct.name}
     </div>
     <div class="product-price">
-       ${matchingProduct.priceCents/100}
+       ${formatCurrency( matchingProduct.priceCents)}
     </div>
     <div class="product-quantity">
         <span>
@@ -33,7 +34,7 @@ cartSummaryHtml+=
         </span>
         <span class="update-quantity-link link-primary">
         Update
-        </span>
+        </span>f
         <span class="delete-quantity-link link-primary">
         Delete
         </span>
@@ -47,7 +48,7 @@ cartSummaryHtml+=
     <div class="delivery-option">
         <input type="radio" checked
         class="delivery-option-input"
-        name="delivery-option-1">
+        name="delivery-option-${matchingProduct.id}">
         <div>
         <div class="delivery-option-date">
             Tuesday, June 21
@@ -60,7 +61,7 @@ cartSummaryHtml+=
     <div class="delivery-option">
         <input type="radio"
         class="delivery-option-input"
-        name="delivery-option-1">
+        name="${matchingProduct.id}">
         <div>
         <div class="delivery-option-date">
             Wednesday, June 15
@@ -73,7 +74,7 @@ cartSummaryHtml+=
     <div class="delivery-option">
         <input type="radio"
         class="delivery-option-input"
-        name="delivery-option-1">
+        name="${matchingProduct.id}">
         <div>
         <div class="delivery-option-date">
             Monday, June 13
